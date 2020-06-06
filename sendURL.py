31cbce976ace1d115f66bc5d28536e81c93cdbc4
icakaxd <icakad@icloud.com>
2020-06-06 22:48:38 +0300

    This is Python 3 application for receiving URLs in the local network. I made an iOS Application for Pythonista3 to go with this.

--- sendURL.py
+++ sendURL.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
-"""A client for sending URLs to my PC."""
-import socket
 import sys
+import socket
+import appex
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 60606  # The port used by the server
-
-if len(sys.argv) == 1:
-    url = input("Please enter a URL. http(s)://")
+if appex.get_url():
+	url = appex.get_url()
+elif sys.argv[1]:
+	url = sys.argv[1]
 else:
-    url = sys.argv[1]
+	url = input()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(bytes(url, 'utf-8'))
-    data = s.recv(1024)
-
-print('Received', repr(data.decode('utf-8')))
+s = socket.socket()
+s.connect(('192.168.0.100', 60606))
+s.sendall(url.encode())
+s.close()
